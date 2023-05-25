3faa058fdb6568a1ebbbbeb08f1765c588b4ca83
vishyvienda254 <vishynganga@gmail.com>
2023-05-25 03:59:57 -0700

    all task solved

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9.
+ *
+ * Return: nothing on success
+ */
+void print_numbers(void)
+{
+	int start = 0, end = 9;
+
+	while (start <= end)
+	{
+		_putchar(start + '0');
+		start++;
+	}
+	_putchar('\n');
+}
